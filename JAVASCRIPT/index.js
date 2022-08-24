b826bfc06c7425786b34c0234fc462876284e167
Suganya Natarajan <suganya.natarajan@solitontech.com>
2022-08-24 15:12:22 +0530

    feat:javascript:Update the temperature for next 5 hours

--- JAVASCRIPT/index.js
+++ JAVASCRIPT/index.js
@@ -13,8 +13,17 @@ const date = document.getElementsByClassName("date-style");
 const time = document.getElementsByClassName("time-color");
 
 function cityname_fetch() {
+  let check_cityname = (cityname) => {
+    for (let name of cityname_list) {
+      if (cityname == name) {
+        return true;
+      }
+    }
+    return false;
+  };
 
   cityname = document.getElementById("browser").value.toLowerCase();
+  if (check_cityname(cityname)) {
     const image_path = "./ASSETS/" + cityname + ".svg";
     document.getElementById("icon").src = image_path;
 
@@ -37,7 +46,7 @@ function cityname_fetch() {
       "Nov",
       "Dec",
     ];
-  
+
     let date_of_city = () => {
       if (city_date[1] >= 1 && city_date[1] <= 9) {
         return `0${city_date[1]}-  ${month[city_date[0] - 1]}-  ${
@@ -48,7 +57,6 @@ function cityname_fetch() {
     };
     city_date = date_of_city();
     date[0].innerHTML = city_date;
-
 
     city_time = city_time.split(":");
     time[0].innerHTML =
@@ -86,4 +94,75 @@ function cityname_fetch() {
     document.getElementById("precipitation_percentage").innerHTML =
       precipitation_value.slice(0, precipitation_value.length - 1) + " %";
 
+    document.getElementById("present_temperature").innerHTML =
+      temperature_celsius[0];
+
+    let time_iterator = city_time[0];
+    let ampm = "",
+      time_value;
+    for (var count = 1; count <= 5; count++) {
+      let id_name = `time-after-${count}hour`;
+      time_iterator++;
+      time_value = time_iterator;
+      if (time_value == 12) {
+        ampm = "PM";
+      } else if (time_value > 12) {
+        time_value = time_value - 12;
+        ampm = "PM";
+      } else ampm = "AM";
+      document.getElementById(id_name).innerHTML = time_value + ampm;
+    }
+
+    let temp_image = document.getElementsByClassName("cloud-img");
+    function image_source(temp_after_every_hour, temp_icon) {
+      if (temp_after_every_hour >= 23 && temp_after_every_hour <= 29)
+        document.getElementById(temp_icon).src = "./ASSETS/cloudyIcon.svg";
+      else if (temp_after_every_hour < 18)
+        document.getElementById(temp_icon).src = "./ASSETS/rainyIcon.svg";
+      else if (temp_after_every_hour >= 18 && temp_after_every_hour <= 22)
+        document.getElementById(temp_icon).src = "./ASSETS/windyIcon.svg";
+      else if (temp_after_every_hour > 29)
+        document.getElementById(temp_icon).src = "./ASSETS/sunnyIcon.svg";
+    }
+
+    image_source(temperature_celsius[0], "icon_based_present_temp");
+
+    let temp_list = weather_data[cityname].nextFiveHrs;
+    for (var count = 1; count <= 5; count++) {
+      let celsius_temperature = `temp-after-${count}hour`;
+      let temp_icon = `icon_based_tempafter-${count}hour`;
+
+      let temp_after_every_hour = temp_list[count - 1].slice(
+        0,
+        temp_list[count - 1].length - 2
+      );
+      document.getElementById(celsius_temperature).innerHTML =
+        temp_after_every_hour;
+
+      image_source(temp_after_every_hour, temp_icon);
+    }
+  } else {
+    document.getElementById("icon").src = "./ASSETS/warning.svg";
+    date[0].innerHTML = "NIL";
+    time[0].innerHTML = "NIL";
+    document.getElementById("amimg").src = "./ASSETS/warning.svg";
+    document.getElementById("temp-celsius").innerHTML = "NIL";
+    document.getElementById("temp-farenheit").innerHTML = "NIL";
+    document.getElementById("humidity_percentage").innerHTML = "NIL";
+    document.getElementById("precipitation_percentage").innerHTML = "NIL";
+    document.getElementsByClassName("cloud-img").src = "./ASSETS/warning.svg";
+    document.getElementById("present_temperature").innerHTML = "nil";
+    document.getElementById("present_time").innerHTML='nil';
+    document.getElementById("icon_based_present_temp").src="./ASSETS/warning.svg";
+    for (var count = 1; count <= 5; count++) {
+      let id_name = `time-after-${count}hour`;
+      document.getElementById(id_name).innerHTML ='nil';
+    }
+    for (var count = 1; count <= 5; count++) {
+      let celsius_temperature = `temp-after-${count}hour`;
+      let temp_icon = `icon_based_tempafter-${count}hour`;
+      document.getElementById(celsius_temperature).innerHTML ='nil';
+      document.getElementById(temp_icon).src="./ASSETS/warning.svg";
+    }
+  }
 }
