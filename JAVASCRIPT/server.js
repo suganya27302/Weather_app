4a32b9cb482b146f7a3f82ffd3a89376e0af0529
Suganya Natarajan <suganya.natarajan@solitontech.com>
2022-09-28 15:29:26 +0530

    feat:nodejs:render the html through http server

--- JAVASCRIPT/server.js
+++ JAVASCRIPT/server.js
@@ -1,11 +1,16 @@
 const http = require("http");
 const fs = require("fs");
 const path = require("path");
-
+const timeZone = require("../DATA/timeZone.js");
+const url = require("url");
+let startTime = new Date();
+let dayCheck = 4;
+let weatherData;
+let cityName;
+let nextFiveHrsTemp;
 http
   .createServer((request, response) => {
     let filePath = `..${request.url}`;
-    console.log(filePath);
     if (request.url === "/") {
       filePath = "../index.html";
     }
@@ -20,13 +25,12 @@ http
       ".svg": "image/svg+xml",
     };
     const contentType = mimeTypes[extname] ?? "application/octet-stream";
+
     fs.readFile(filePath, (error, content) => {
       if (error) {
         if (error.code === "ENOENT") {
-          fs.readFile("./404.html", (error, content) => {
-            response.writeHead(404, { "Content-Type": "text/html" });
-            response.end(content, "utf-8");
-          });
+          response.writeHead(404, { "Content-Type": "text/plain" });
+          response.end("404 page not found");
         } else {
           response.writeHead(500);
           response.end(
@@ -34,7 +38,6 @@ http
           );
         }
       } else {
-        console.log(extname + " " + contentType);
         response.writeHead(200, { "Content-Type": contentType });
         response.end(content, "utf-8");
       }
