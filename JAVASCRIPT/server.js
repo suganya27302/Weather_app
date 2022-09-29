065e424b1bf3b242f348ceb1574e62d9d8266686
Suganya Natarajan <suganya.natarajan@solitontech.com>
2022-09-29 15:55:19 +0530

    feat:nodejs:add documentation to server.js

--- JAVASCRIPT/server.js
+++ JAVASCRIPT/server.js
@@ -11,7 +11,11 @@ let weatherData = [];
 let cityName;
 let nextFiveHrsTemp;
 
-// local server creation
+/**
+ * Local server to handle request and response back to the client.
+ * if the request is invalid it display a error message
+ *  orelse it fetch the data and return response.
+ * */
 http
   .createServer((request, response) => {
     let currentTime = new Date();
@@ -20,14 +24,17 @@ http
     const SearchParams = new URLSearchParams(cityInfo);
     let nameOfTheCity = SearchParams.get("city");
 
-    // Respond to the request to fetch all cities data
+    /**
+     * Respond to the request to fetch all cities data.
+     * Every four hours once, it will update the all cities data and
+     * respond to the client.
+     */
     if (request.url == "/all-timezone-cities") {
       let differenceTime = currentTime - startTime;
 
       if (differenceTime > dayCheck) {
         startTime = new Date();
         weatherData = timeZone.allTimeZones();
-        //console.log(weatherData);
         response.write(JSON.stringify(weatherData));
         response.end();
       } else {
@@ -37,10 +44,13 @@ http
         response.write(JSON.stringify(weatherData));
         response.end();
       }
-    }
-
-    // Respond to the request to fetch the city information
-    if (query == "/?city") {
+    } else if (query == "/?city") {
+      /**
+       * Respond to the request to fetch the city information
+       * When the url is with a city query, it fetch the selected city information
+       * and respond back to the client.
+       * If it unable to call the function it displays error message to page.
+       */
       if (nameOfTheCity) {
         cityName = timeZone.timeForOneCity(nameOfTheCity);
         response.write(JSON.stringify(cityName));
@@ -49,10 +59,13 @@ http
         response.writeHead(404);
         response.end("Not a Valid Endpoint. Please check API doc");
       }
-    }
-
-    // Respond to the request to fetch next five hours temperature value
-    if (request.url == "/hourly-forecast" && request.method == "POST") {
+    } else if (request.url == "/hourly-forecast" && request.method == "POST") {
+      /**
+       * When the url with hourly-forecast and post request
+       * it capture the chunk data and used as a parameter to fetch the next five hours temperature
+       * and respond back to the client.
+       * If it unable to call the function it displays error message to page.
+       * */
       let cityData = "";
       request.on("data", (chunk) => {
         cityData += chunk.toString();
@@ -72,41 +85,50 @@ http
           response.end("Not a Valid Endpoint. Please check API doc");
         }
       });
-    }
-    // HTTP request to render the HTML file when the user enters the specific port
-
-    let filePath = `..${request.url}`;
-    if (request.url === "/") {
-      filePath = "../index.html";
-    }
-    const extname = String(path.extname(filePath)).toLowerCase();
-    const mimeTypes = {
-      ".html": "text/html",
-      ".js": "text/javascript",
-      ".css": "text/css",
-      ".png": "image/png",
-      ".jpg": "image/jpg",
-      ".gif": "image/gif",
-      ".svg": "image/svg+xml",
-    };
-    const contentType = mimeTypes[extname] ?? "application/octet-stream";
-
-    fs.readFile(filePath, (error, content) => {
-      if (error) {
-        if (error.code === "ENOENT") {
-          response.writeHead(404, { "Content-Type": "text/plain" });
-          response.end("404 page not found");
-        } else {
-          response.writeHead(500);
-          response.end(
-            `Sorry, check with the site admin for error: ${error.code} ..\n`
-          );
-        }
-      } else {
-        response.writeHead(200, { "Content-Type": contentType });
-        response.end(content, "utf-8");
+    } else {
+    /**
+     * HTTP request to render the HTML file when the user enters the specific port
+     */
+      let filePath = `..${request.url}`;
+      // It renders the index page when the url enters.
+      if (request.url === "/") {
+        filePath = "../index.html";
       }
-    });
+      const extname = String(path.extname(filePath)).toLowerCase();
+      const mimeTypes = {
+        ".html": "text/html",
+        ".js": "text/javascript",
+        ".css": "text/css",
+        ".png": "image/png",
+        ".jpg": "image/jpg",
+        ".gif": "image/gif",
+        ".svg": "image/svg+xml",
+      };
+      // if the extension type is not in object , default content type is taken.
+      const contentType = mimeTypes[extname] ?? "application/octet-stream";
+
+      fs.readFile(filePath, (error, content) => {
+        if (error) {
+          // when there is issue with file directories path it executes.
+          if (error.code === "ENOENT") {
+            response.writeHead(404, { "Content-Type": "text/plain" });
+            response.end("404 page not found");
+          }
+          // when there is issue with server in accessing file , it executes.
+          else {
+            response.writeHead(500);
+            response.end(
+              `Sorry, check with the site admin for error: ${error.code} ..\n`
+            );
+          }
+        }
+        // It executes, when the content type matches with the extension.
+        else {
+          response.writeHead(200, { "Content-Type": contentType });
+          response.end(content, "utf-8");
+        }
+      });
+    }
   })
   .listen(8125);
 
